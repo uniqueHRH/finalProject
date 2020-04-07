5360677d106f01719ec1f102ebc359ff922fcb5b
unique <lipglow@naver.com>
2020-04-07 20:43:38 +0900

    PAGE

--- project/src/main/java/com/bit/project/common/Pagination.java
+++ project/src/main/java/com/bit/project/common/Pagination.java
@@ -8,7 +8,7 @@ public class Pagination {
 	private int listSize=10;   // 초기값으로 목록개수를 10으로 셋팅
 	private int rangeSize=5;   // 초기값으로 페이지범위를 5로 셋팅 (페이징 갯수)
 	private int page;   // 현재 목록의 페이지 번호
-	private int range;   // 각 페이지 범위 시작 번호
+	private int range;   // 페이지 (1~5 / 6~10)
 	private int listCnt;   // 전체 개시물의 개수
 	private int pageCnt;   // 전체 페이지 범위의 개수
 	private int startPage;   // 각 페이지 범위 시작 번호
@@ -16,13 +16,11 @@ public class Pagination {
 	private int endPage;   // 각 페이지 범위 끝 번호
 	private boolean prev;
 	private boolean next;
-
 	
+
 
 	public Pagination() {
 	}
-
-	
 	public int getRangeSize() {
 		return rangeSize;
 	}
@@ -81,28 +79,28 @@ public class Pagination {
 		this.page = page;
 		this.range = range;
 		this.listCnt = listCnt;
-
+		
 		//전체 페이지수 
-		this.pageCnt = (int) Math.ceil(listCnt/listSize);
-
+		this.pageCnt = (int) Math.ceil(listCnt/listSize)+1;
+		
 		//시작 페이지
-		this.startPage = (range - 1) * rangeSize + 1 ;
-
+		this.startPage = (range - 1) * rangeSize + 1;
+		
 		//끝 페이지
 		this.endPage = range * rangeSize;
-
+		
 		//게시판 시작번호
 		this.startList = (page - 1) * listSize;
-
+		
 		//이전 버튼 상태
-		this.prev = range == 1 ? false : true;
-
+		this.prev=range==1?false:true;
+		
 		//다음 버튼 상태
-		this.next = endPage > pageCnt ? false : true;
-		if (this.endPage > this.pageCnt) {
-			this.endPage = this.pageCnt;
-			this.next = false;
-
+		this.next=endPage>pageCnt?false:true;
+		
+		if (this.endPage>this.pageCnt) {
+			this.endPage=this.pageCnt;
+			this.next=false;
 		}
 
 	}
